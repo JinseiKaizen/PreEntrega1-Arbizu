b50e6c3c19fe791211b31083e794766ad15a12cf
ALDO ALBERTO ARBIZU <69614658+JinseiKaizen@users.noreply.github.com>
2023-03-30 12:55:12 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import NavBarComponent from './components/NavBarComponent';
+
+function App() {
+  return (
+    <NavBarComponent />
+       
+  );
+}
+
+export default App;
